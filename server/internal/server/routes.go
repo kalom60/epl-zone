794ad7d8685c7d8f3669a8ee3f4670b7f765a676
kalom60 <39136564+kalom60@users.noreply.github.com>
2024-06-16 21:58:06 +0300

    sample routes

--- server/internal/server/routes.go
+++ server/internal/server/routes.go
@@ -3,6 +3,7 @@ package server
 import (
 	"net/http"
 
+	"github.com/kalom60/epl-zone/internal/scrape"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
@@ -12,9 +13,15 @@ func (s *Server) RegisterRoutes() http.Handler {
     e.Use(middleware.Logger())
     e.Use(middleware.Recover())
 
+    e.GET("/", s.scrapeHandler)
     e.GET("/health", s.healthHandler)
 
     return e
+}
+
+func (s *Server) scrapeHandler(c echo.Context) error {
+    scrape.Scrapper()
+    return c.JSON(http.StatusOK, "Done")
 }
 
 func (s *Server) healthHandler(c echo.Context) error {
