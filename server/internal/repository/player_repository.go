650e1a65dd283839c53587220b0ea65de76367ba
kalom60 <39136564+kalom60@users.noreply.github.com>
2024-06-18 22:04:13 +0300

    Postgres repository

---
+++ server/internal/repository/player_repository.go
@@ -0,0 +1,7 @@
+package repository
+
+import "github.com/kalom60/epl-zone/internal/models"
+
+type PlayerRepository interface {
+	GetAllPlayers() (*models.Player, error)
+}
---
+++ server/internal/repository/player_repository_pg.go
@@ -0,0 +1,25 @@
+package repository
+
+import (
+	"github.com/jmoiron/sqlx"
+	"github.com/kalom60/epl-zone/internal/models"
+)
+
+type PgPlayerRepository struct {
+	db *sqlx.DB
+}
+
+func NewPgPlayerRepository(db *sqlx.DB) PlayerRepository {
+	return &PgPlayerRepository{db: db}
+}
+
+func (r *PgPlayerRepository) GetAllPlayers() (*models.Player, error) {
+    var players models.Player
+
+    err := r.db.Select(&players, "SELECT * FROM eplzone")
+    if err != nil {
+        return nil, err
+    }
+
+    return &players, nil
+}
