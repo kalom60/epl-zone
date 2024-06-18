6bf6604062451c3d21c12941e22a37b1c6edd847
kalom60 <39136564+kalom60@users.noreply.github.com>
2024-06-18 22:05:55 +0300

    converting CSV to db record, creating table

--- server/internal/database/database.go
+++ server/internal/database/database.go
@@ -14,6 +14,8 @@ type Service interface {
 	DB() *sqlx.DB
 	Health() map[string]string
 	Close() error
+	ConvertCSVToDB() error
+    FlushPlayerTable() error
 }
 
 type service struct {
@@ -29,22 +31,27 @@ var (
 	dbInstace *service
 )
 
-func New() Service {
+func New() (Service, error) {
 	if dbInstace != nil {
-		return dbInstace
+		return dbInstace, nil
 	}
 
 	connStr := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable", username, password, host, port, database)
 	db, err := sqlx.Connect("pgx", connStr)
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
 
 	dbInstace = &service{
 		db: db,
 	}
 
-	return dbInstace
+	if err := dbInstace.CreatePlayerTable(); err != nil {
+		db.Close()
+		return nil, fmt.Errorf("failed to create database tables: %w", err)
+	}
+
+	return dbInstace, nil
 }
 
 func (s *service) DB() *sqlx.DB {
@@ -65,4 +72,59 @@ func (s *service) Health() map[string]string {
 func (s *service) Close() error {
 	log.Printf("Disconnected from database: %s", database)
 	return s.db.Close()
+}
+
+func (s *service) ConvertCSVToDB() error {
+	filePath := "data/stats.csv"
+
+	_, err := os.Stat(filePath)
+	if os.IsNotExist(err) {
+		return fmt.Errorf("CSV file %s does not exist", filePath)
+	}
+
+	_, err = s.db.Exec(`COPY player_data FROM 'data/stats.csv' DELIMITER ',' CSV HEADER`)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (s *service) CreatePlayerTable() error {
+	createTableQuery := `
+        CREATE TABLE IF NOT EXISTS players (
+            player_name VARCHAR(255) NOT NULL,
+            nation VARCHAR(255),
+            position VARCHAR(255),
+            age FLOAT,
+            matches_played INT,
+            starts INT,
+            minutes_played FLOAT,
+            goals FLOAT,
+            assists FLOAT,
+            penalities_scored FLOAT,
+            yellow_cards FLOAT,
+            red_cards FLOAT,
+            expected_goals FLOAT,
+            expected_assists FLOAT,
+            team_name VARCHAR(255),
+            PRIMARY KEY (player_name)
+        );
+    `
+	_, err := s.db.Exec(createTableQuery)
+	if err != nil {
+		return fmt.Errorf("failed to create players table: %w", err)
+	}
+	return nil
+}
+
+func (s *service) FlushPlayerTable() error {
+	flushTableQuery := `
+        DELETE FROM players;
+    `
+	_, err := s.db.Exec(flushTableQuery)
+	if err != nil {
+		return err
+	}
+	return nil
 }
