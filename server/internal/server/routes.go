a7124819185e6fe582fe0900d59975a2a2452c91
kalom60 <39136564+kalom60@users.noreply.github.com>
2024-06-21 20:30:53 +0300

    remove / route

--- server/internal/server/routes.go
+++ server/internal/server/routes.go
@@ -3,29 +3,22 @@ package server
 import (
 	"net/http"
 
-	"github.com/kalom60/epl-zone/internal/scrape"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
 
 func (s *Server) RegisterRoutes() http.Handler {
-    e := echo.New()
-    e.Use(middleware.Logger())
-    e.Use(middleware.Recover())
+	e := echo.New()
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
 
-    e.GET("/", s.scrapeHandler)
-    e.GET("/health", s.healthHandler)
+	e.GET("/health", s.healthHandler)
 
-    e.GET("/allplayers", s.playerHandler.GetAllPlayers)
+	e.GET("/allplayers", s.playerHandler.GetAllPlayers)
 
-    return e
-}
-
-func (s *Server) scrapeHandler(c echo.Context) error {
-    scrape.Scrapper()
-    return c.JSON(http.StatusOK, "Done")
+	return e
 }
 
 func (s *Server) healthHandler(c echo.Context) error {
-    return c.JSON(http.StatusOK, s.db.Health())
+	return c.JSON(http.StatusOK, s.db.Health())
 }
