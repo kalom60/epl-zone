081738a24b7ab38549e47255ca9c1e83296a8142
kalom60 <39136564+kalom60@users.noreply.github.com>
2024-06-22 14:51:55 +0300

    using nulls

--- server/internal/models/player.go
+++ server/internal/models/player.go
@@ -1,22 +1,24 @@
 package models
 
-import "database/sql"
+import (
+	"github.com/kalom60/epl-zone/pkg/nulls"
+)
 
 type Player struct {
-	ID              int             `json:"id" db:"id"`
-	Player          string          `json:"player" db:"player"`
-	Nation          sql.NullString  `json:"nation" db:"nation"`
-	Position        sql.NullString  `json:"position" db:"pos"`
-	Age             sql.NullFloat64 `json:"age" db:"age"`
-	MatchesPlayed   sql.NullInt64   `json:"matchesPlayed" db:"mp"`
-	Starts          sql.NullInt64   `json:"starts" db:"starts"`
-	MinutesPlayed   sql.NullFloat64 `json:"minutesPlayed" db:"min"`
-	Goals           sql.NullFloat64 `json:"goals" db:"gls"`
-	Assists         sql.NullFloat64 `json:"assists" db:"ast"`
-	PenaltiesScored sql.NullFloat64 `json:"penalitiesScored" db:"pk"`
-	YellowCards     sql.NullFloat64 `json:"yellowCards" db:"crdy"`
-	RedCards        sql.NullFloat64 `json:"redCards" db:"crdr"`
-	ExpectedGoals   sql.NullFloat64 `json:"expectedGoals" db:"xg"`
-	ExpectedAssists sql.NullFloat64 `json:"expectedAssists" db:"xag"`
-	TeamName        string          `json:"teamName" db:"team"`
+	ID              int               `json:"id" db:"id"`
+	Player          string            `json:"player" db:"player"`
+	Nation          nulls.NullString  `json:"nation" db:"nation"`
+	Position        nulls.NullString  `json:"position" db:"pos"`
+	Age             nulls.NullFloat64 `json:"age" db:"age"`
+	MatchesPlayed   nulls.NullInt64   `json:"matchesPlayed" db:"mp"`
+	Starts          nulls.NullInt64   `json:"starts" db:"starts"`
+	MinutesPlayed   nulls.NullFloat64 `json:"minutesPlayed" db:"min"`
+	Goals           nulls.NullFloat64 `json:"goals" db:"gls"`
+	Assists         nulls.NullFloat64 `json:"assists" db:"ast"`
+	PenaltiesScored nulls.NullFloat64 `json:"penalitiesScored" db:"pk"`
+	YellowCards     nulls.NullFloat64 `json:"yellowCards" db:"crdy"`
+	RedCards        nulls.NullFloat64 `json:"redCards" db:"crdr"`
+	ExpectedGoals   nulls.NullFloat64 `json:"expectedGoals" db:"xg"`
+	ExpectedAssists nulls.NullFloat64 `json:"expectedAssists" db:"xag"`
+	TeamName        string            `json:"teamName" db:"team"`
 }
