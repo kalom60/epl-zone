f40bb0a94708df000aa8b2350d7867964408f0eb
kalom60 <39136564+kalom60@users.noreply.github.com>
2024-06-22 13:56:09 +0300

    get players repo by team and position

--- server/internal/repository/player_repository.go
+++ server/internal/repository/player_repository.go
@@ -4,4 +4,6 @@ import "github.com/kalom60/epl-zone/internal/models"
 
 type PlayerRepository interface {
 	GetAllPlayers() (*[]models.Player, error)
+	GetPlayersByTeam(string) (*[]models.Player, error)
+	GetPlayersByPostiton(string) (*[]models.Player, error)
 }
--- server/internal/repository/player_repository_pg.go
+++ server/internal/repository/player_repository_pg.go
@@ -23,3 +23,25 @@ func (r *PgPlayerRepository) GetAllPlayers() (*[]models.Player, error) {
 
 	return &players, nil
 }
+
+func (r *PgPlayerRepository) GetPlayersByTeam(team string) (*[]models.Player, error) {
+	var players []models.Player
+
+	err := r.db.Select(&players, "SELECT * FROM players WHERE team=$1", team)
+	if err != nil {
+		return nil, err
+	}
+
+	return &players, nil
+}
+
+func (r *PgPlayerRepository) GetPlayersByPostiton(pos string) (*[]models.Player, error) {
+	players := []models.Player{}
+
+	err := r.db.Select(&players, "SELECT * FROM players WHERE pos=$1", pos)
+	if err != nil {
+		return nil, err
+	}
+
+	return &players, nil
+}
