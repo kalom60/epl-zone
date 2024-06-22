e2c14818eb566be1dcd98c1065bc80926d2d7b93
kalom60 <39136564+kalom60@users.noreply.github.com>
2024-06-22 13:56:27 +0300

    get players handler by team and position

--- server/internal/handlers/player_handlers.go
+++ server/internal/handlers/player_handlers.go
@@ -9,6 +9,8 @@ import (
 
 type PlayerHandlers interface {
 	GetAllPlayers(c echo.Context) error
+	GetPlayersByTeam(c echo.Context) error
+	GetPlayersByPostiton(c echo.Context) error
 }
 
 type PlayerHandler struct {
@@ -21,6 +23,28 @@ func NewPlayerHandler(playerRepo repository.PlayerRepository) PlayerHandlers {
 
 func (h *PlayerHandler) GetAllPlayers(c echo.Context) error {
 	players, err := h.playerRepo.GetAllPlayers()
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]string{"message": "Something went wrong"})
+	}
+
+	return c.JSON(http.StatusOK, players)
+}
+
+func (h *PlayerHandler) GetPlayersByTeam(c echo.Context) error {
+	team := c.Param("team")
+
+	players, err := h.playerRepo.GetPlayersByTeam(team)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]string{"message": "Something went wrong"})
+	}
+
+	return c.JSON(http.StatusOK, players)
+}
+
+func (h *PlayerHandler) GetPlayersByPostiton(c echo.Context) error {
+	position := c.Param("position")
+
+	players, err := h.playerRepo.GetPlayersByPostiton(position)
 	if err != nil {
 		return c.JSON(http.StatusInternalServerError, map[string]string{"message": "Something went wrong"})
 	}
