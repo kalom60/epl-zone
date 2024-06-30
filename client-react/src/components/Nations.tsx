3873c19047e42e78b8fb3c0ffa570278f027993e
kalom60 <39136564+kalom60@users.noreply.github.com>
2024-06-30 17:30:15 +0300

    nations component

---
+++ client-react/src/components/Nations.tsx
@@ -0,0 +1,13 @@
+import SearchBox from "./SearchBox";
+
+const Nations = () => {
+  return (
+    <div className="container mt-8">
+      <div className="md:mx-48">
+        <SearchBox title="Nations" placeholder="Search for nations" />
+      </div>
+    </div>
+  );
+};
+
+export default Nations;
