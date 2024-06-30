17bb1d97893eb1770b2733e68dd0fe28a73c0763
kalom60 <39136564+kalom60@users.noreply.github.com>
2024-06-30 22:34:12 +0300

    fetch teams

--- client-react/src/components/Teams.tsx
+++ client-react/src/components/Teams.tsx
@@ -1,10 +1,40 @@
+import { useEffect, useState } from "react";
 import SearchBox from "./SearchBox";
 
+type State = {
+  id: string;
+  team: string;
+  logo: string;
+};
+
 const Teams = () => {
+  const [teams, setTeams] = useState<State[]>([]);
+
+  const getTeams = async () => {
+    const respose = await fetch("http://localhost:8080/teams");
+    const data = await respose.json();
+    setTeams(data);
+  };
+
+  useEffect(() => {
+    getTeams();
+  }, []);
+
   return (
     <div className="container mt-8">
       <div className="md:mx-48">
         <SearchBox title="Teams" placeholder="Search for teams" />
+      </div>
+
+      <div className="grid grid-cols-4 gap-4 place-items-center mt-20">
+        {teams.length > 0 &&
+          teams.map((team: State) => {
+            return (
+              <div className="my-4">
+                <h1>{team.team}</h1>
+              </div>
+            );
+          })}
       </div>
     </div>
   );
