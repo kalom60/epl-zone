838429aea69bb8b91a92dc0e7b24c2253e5b18de
kalom60 <39136564+kalom60@users.noreply.github.com>
2024-06-30 17:28:42 +0300

    teams component

---
+++ client-react/src/components/Teams.tsx
@@ -0,0 +1,13 @@
+import SearchBox from "./SearchBox";
+
+const Teams = () => {
+  return (
+    <div className="container mt-8">
+      <div className="md:mx-48">
+        <SearchBox title="Teams" placeholder="Search for teams" />
+      </div>
+    </div>
+  );
+};
+
+export default Teams;
