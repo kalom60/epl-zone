f12333fa04d14800c37f9c64f08da83a9864928a
kalom60 <39136564+kalom60@users.noreply.github.com>
2024-06-30 17:29:09 +0300

    positions component

---
+++ client-react/src/components/Positions.tsx
@@ -0,0 +1,13 @@
+import SearchBox from "./SearchBox";
+
+const Positions = () => {
+  return (
+    <div className="container mt-8">
+      <div className="md:mx-48">
+        <SearchBox title="Positions" placeholder="Search for positions" />
+      </div>
+    </div>
+  );
+};
+
+export default Positions;
