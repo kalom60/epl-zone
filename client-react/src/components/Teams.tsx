b55330177f6ded6fedfb3b9e3d0ef83f19c5502a
kalom60 <39136564+kalom60@users.noreply.github.com>
2024-07-01 09:27:46 +0300

    use normal logo size

--- client-react/src/components/Teams.tsx
+++ client-react/src/components/Teams.tsx
@@ -29,8 +29,12 @@ const Teams = () => {
       <div className="grid grid-cols-4 gap-4 place-items-center mt-20">
         {teams.length > 0 &&
           teams.map((team: State) => {
+            const arr: string[] = team.logo.split("mini.");
+            const logo = arr.join("");
+
             return (
               <div className="my-4">
+                <img src={logo} />
                 <h1>{team.team}</h1>
               </div>
             );
