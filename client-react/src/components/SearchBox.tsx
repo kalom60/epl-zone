6815479003b8c2608c3476a2bcf6554ec9b47dd6
kalom60 <39136564+kalom60@users.noreply.github.com>
2024-07-02 09:09:38 +0300

    add searching parent component

--- client-react/src/components/SearchBox.tsx
+++ client-react/src/components/SearchBox.tsx
@@ -1,16 +1,26 @@
 import { Input } from "@/components/ui/input";
-import React from "react";
+import React, { ChangeEvent } from "react";
 
 type Props = {
   title: string;
   placeholder: string;
+  onSearch: (query: string) => void;
 };
 
-const SearchBox: React.FC<Props> = ({ title, placeholder }) => {
+const SearchBox: React.FC<Props> = ({ title, placeholder, onSearch }) => {
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    onSearch(e.target.value);
+  };
+
   return (
     <>
-      <h1 className="text-4xl">{title}</h1>
-      <Input type="text" className="mt-6 border-2" placeholder={placeholder} />
+      <h1 className="text-4xl font-semibold">{title}</h1>
+      <Input
+        type="text"
+        className="mt-6 border-2 p-5"
+        placeholder={placeholder}
+        onChange={handleInputChange}
+      />
     </>
   );
 };
