f8e0adcae0fbb1690416dc8c7ffb216a11dc2bfa
kalom60 <39136564+kalom60@users.noreply.github.com>
2024-07-02 11:14:27 +0300

    utils

---
+++ client-react/src/utils/columns.tsx
@@ -0,0 +1,108 @@
+import { ColumnDef } from "@tanstack/react-table";
+import { ArrowUpDown } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+
+export type Data = {
+  id: string;
+  player: string;
+  nation: string;
+  position: string;
+  age: number;
+  matchesPlayed: number;
+  starts: number;
+  minutesPlayed: number;
+  goals: number;
+  assists: number;
+  penalitiesScored: number;
+  yellowCards: number;
+  redCards: number;
+  expectedGoals: number;
+  expectedAssists: number;
+  teamName: string;
+};
+
+const columnConfig = [
+  {
+    key: "player",
+    header: "Player",
+    className: "capitalize",
+    sortable: true,
+  },
+  {
+    key: "nation",
+    header: "Nation",
+  },
+  {
+    key: "position",
+    header: "Position",
+    render: (value: string) =>
+      value.length > 3
+        ? `${value.substring(0, 2)},${value.substring(2)}`
+        : value,
+  },
+  {
+    key: "age",
+    header: "Age",
+  },
+  {
+    key: "matchesPlayed",
+    header: "Matches Played",
+  },
+  {
+    key: "starts",
+    header: "Starts",
+  },
+  {
+    key: "minutesPlayed",
+    header: "Minutes Played",
+  },
+  {
+    key: "goals",
+    header: "Goals",
+  },
+  {
+    key: "assists",
+    header: "Assists",
+  },
+  {
+    key: "penalitiesScored",
+    header: "Penalities Scored",
+  },
+  {
+    key: "yellowCards",
+    header: "Yellow Cards",
+  },
+  {
+    key: "redCards",
+    header: "Red Cards",
+  },
+  {
+    key: "expectedGoals",
+    header: "Expected Goals",
+  },
+  {
+    key: "expectedAssists",
+    header: "Expected Assists",
+  },
+];
+
+export const columns: ColumnDef<Data>[] = columnConfig.map((col) => ({
+  accessorKey: col.key,
+  header: col.sortable
+    ? ({ column }) => (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          {col.header}
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      )
+    : col.header,
+  cell: ({ row }) => (
+    <div className={col.className}>
+      {col.render ? col.render(row.getValue(col.key)) : row.getValue(col.key)}
+    </div>
+  ),
+}));
